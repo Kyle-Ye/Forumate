f2345965250f5fe47dc5db42b3a29fe432be53cb
Kyle <kyle201817146@gmail.com>
2023-04-19 15:18:32 +0800

    Init commit

---
+++ Forumate/ForumateApp.swift
@@ -0,0 +1,17 @@
+//
+//  ForumateApp.swift
+//  Forumate
+//
+//  Created by Kyle on 2023/4/19.
+//
+
+import SwiftUI
+
+@main
+struct ForumateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
