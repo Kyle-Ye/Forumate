7e777180a154c1b47183985f9c606c5409900df4
Kyle <kyle201817146@gmail.com>
2023-05-23 00:27:28 +0800

    Add Topic Label

--- Forumate/PreviewData/Topic+Preview.swift
+++ Forumate/PreviewData/Topic+Preview.swift
@@ -10,12 +10,44 @@ import DiscourseKit
 
 extension Topic {
     static var test: Topic {
-        let decoder = JSONDecoder()
+        let decoder = JSONDecoder.discourse
         let data = #"""
         {
             "id": 1,
+            "category_id": 1,
             "created_at": "2023-03-19T13:51:09.286Z",
-            "title": "Test Title"
+            "title": "Test Title",
+            "fancy_title": "Test Title",
+            "posts_count": 25,
+            "new_posts": 2,
+            "tags": ["swift-docc"],
+            "pinned": true,
+            "posters": [
+            {
+              "description": "Original Poster",
+              "user_id": -1
+            },
+            {
+              "description": "Frequent Poster",
+              "user_id": 3749
+            },
+            {
+              "description": "Frequent Poster",
+              "user_id": 553824
+            },
+            {
+              "description": "Frequent Poster",
+              "extras": null,
+              "user_id": 558081
+            },
+            {
+              "description": "Most Recent Poster",
+              "extras": "latest",
+              "user_id": 566394
+            }
+            ],
+            "last_posted_at": "2023-03-26T10:08:25.549Z",
+            "last_poster_username": "Nguyen"
         }
         """#.data(using: .utf8)!
         return try! decoder.decode(Topic.self, from: data)
--- Forumate/View/Tab/Topics/Community/CommunityDetail/Topic/TopicLabel.swift
+++ Forumate/View/Tab/Topics/Community/CommunityDetail/Topic/TopicLabel.swift
@@ -5,14 +5,58 @@
 //  Created by Kyle on 2023/5/22.
 //
 
-import SwiftUI
 import DiscourseKit
+import Flow
+import SwiftUI
 
 struct TopicLabel: View {
     let topic: Topic
     
     var body: some View {
-        Text(topic.title)
+        VStack(alignment: .leading, spacing: 4) {
+            mainInfo
+            authorInfo
+            extensionInfo
+        }
+    }
+
+    var mainInfo: some View {
+        HFlow(spacing: 2) {
+            if topic.pinned {
+                Image(systemName: "pin.fill")
+            }
+            Text(topic.title)
+                .bold()
+            ForEach(topic.tags, id: \.self) { tag in
+                TagView(tag)
+            }
+        }
+    }
+    
+    var authorInfo: some View {
+        HStack {
+            Text("by")
+        }
+        .font(.footnote)
+    }
+
+    var extensionInfo: some View {
+        HStack {
+            HStack(spacing: 2) {
+                Image(systemName: "message")
+                Text(topic.postsCount.description)
+            }
+            HStack(spacing: 2) {
+                Image(systemName: "calendar.badge.clock")
+                HStack(spacing: 5) {
+                    Text(topic.lastPostedAt, style: .relative)
+                    Text("·")
+                    Text(topic.lastPosterUsername)
+                }
+            }
+        }
+        .font(.caption)
+        .foregroundColor(.secondary)
     }
 }
 
