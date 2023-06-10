323f855c584a52a49a08e3623c4e713873b3482f
Kyle <kyle201817146@gmail.com>
2023-06-11 03:00:17 +0800

    Update List for watchOS 10

--- Forumate/View/Tab/Topics/ViewModel/TopicsTabState.swift
+++ Forumate/View/Tab/Topics/ViewModel/TopicsTabState.swift
@@ -5,15 +5,13 @@
 //  Created by Kyle on 2023/5/20.
 //
 
-import Foundation
 import DiscourseKit
+import Foundation
 import SwiftUI
 
 class TopicsTabState: ObservableObject {
     @Published var selectedCommunity: Community?
     @Published var selectedTopic: Topic?
     
-    #if os(iOS) || os(macOS) || os(tvOS)
     @Published var columnVisibility: NavigationSplitViewVisibility = .automatic
-    #endif
 }
