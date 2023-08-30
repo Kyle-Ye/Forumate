d061d556e90b9eeff2d98cd3f897f6c158b4a2e0
Kyle <kyle201817146@gmail.com>
2023-08-31 01:48:16 +0800

    Fix visionOS column issue

--- Forumate/View/Tab/Topics/ViewModel/TopicsTabState.swift
+++ Forumate/View/Tab/Topics/ViewModel/TopicsTabState.swift
@@ -14,6 +14,10 @@ class TopicsTabState: ObservableObject {
     @Published var selectedCommunity: Community?
     @Published var selectedCategories: [Category] = []
     @Published var selectedTopic: Topic?
+    #if os(visionOS)
+    @Published var columnVisibility: NavigationSplitViewVisibility = .all
+    #else
     @Published var columnVisibility: NavigationSplitViewVisibility = .automatic
+    #endif
     @Published var column: NavigationSplitViewColumn = .sidebar
 }
