f0504ff03cdb92917456737f0f95708aeead6d0f
Kyle <kyle201817146@gmail.com>
2023-09-01 02:05:58 +0800

    [Optimize] Update columnVisibility for all platform

--- Forumate/View/Tab/Topics/ViewModel/TopicsTabState.swift
+++ Forumate/View/Tab/Topics/ViewModel/TopicsTabState.swift
@@ -14,10 +14,6 @@ class TopicsTabState: ObservableObject {
     @Published var selectedCommunity: Community?
     @Published var selectedCategories: [Category] = []
     @Published var selectedTopic: Topic?
-    #if os(visionOS)
     @Published var columnVisibility: NavigationSplitViewVisibility = .all
-    #else
-    @Published var columnVisibility: NavigationSplitViewVisibility = .automatic
-    #endif
     @Published var column: NavigationSplitViewColumn = .sidebar
 }
